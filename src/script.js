40b93070ad4c7d86d684e842e62e9053f15f868e
daniltarasov <59774430+daniltarasov@users.noreply.github.com>
2020-04-05 00:22:25 +0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,18 +1,53 @@
- area.value = localStorage.getItem('area');
-    area.oninput = () => {
-      localStorage.setItem('area', "Ваш город-"+""+area.value)
-      
-    };
+function start(){
 
-
-
-    [].slice.call(document.querySelectorAll(".check")).forEach(function(el) {
-  el.onchange = function() {
-     localStorage.setItem(el.id, el.checked);
-  };
-  if (localStorage.getItem(el.id) === "true") {
-    el.checked = true;
-    el.disabled = "disabled";
+  area.value = getCookie('area');
+  if (getCookie("saved") === "true") {
+      let checkArr = document.querySelectorAll(".check");
+      checkArr.forEach(function(elem){
+        if (getCookie(elem.id) === "true"){
+          elem.checked = true;
+        }
+        else {
+          elem.checked = false;
+        }
+        elem.disabled = "disabled";
+      });
   }
+  console.log(document.cookie);
+}
 
-})
+function getCookie(c_name) {
+    console.log(document.cookie);
+    let matches = document.cookie.match(new RegExp("(?:^|; )" + c_name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+    return matches ? decodeURIComponent(matches[1]) : "";
+}
+
+function setCookie(name, value, age) {
+    value = encodeURIComponent(name) + '=' + encodeURIComponent(value) + '; max-age=' + age;
+    console.log(value);
+    // document.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value) + ';max-age=' + age;
+    document.cookie = value;
+}
+
+function deleteCookie(name) {
+    setCookie(name, "", "-1");
+}
+
+area.onchange = () => {
+  setCookie('area', "Ваш город-"+""+area.value, 3600)
+};
+
+document.getElementById('save').onclick = function () {
+  deleteCookie("area");
+  area.value = "";
+  console.log(document.cookie);
+}
+
+document.getElementById('saveChkbox').onclick = function () {
+      let checkArr = document.querySelectorAll(".check");
+      checkArr.forEach(function(elem){
+          setCookie(elem.id, elem.checked, 3600);
+      });
+      setCookie("saved", "true")
+      console.log(document.cookie);
+}
